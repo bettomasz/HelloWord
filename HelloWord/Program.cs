c524d7590cf64458acb3a1b342dfa86fed45f15b
Tom <marmarmar719@gmail.com>
2022-10-25 09:41:04 +0200

    dodano klasy test i test3

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Tomuczka ");
+Console.WriteLine("Tomuczka 2");
+Console.WriteLine("Tomuczka 3");
